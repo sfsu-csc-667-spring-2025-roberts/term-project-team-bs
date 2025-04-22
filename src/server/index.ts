4cf60b09827366453637170cb0c4fbf7e378a9df
Nishi <113165182+nishisuratia@users.noreply.github.com>
2025-04-22 11:55:08 -0700

    Add files via upload

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'; 
 import express from 'express';
 import httpErrors from 'http-errors';
 import rootRouter from './routes/roots';
@@ -5,6 +6,10 @@ import { timeMiddleware } from './middleware/time';
 import * as path from "path";
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import * as config from '.'
+
+import { configureSession } from './config';
+// …
 
 
 
@@ -12,6 +17,8 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
@@ -21,7 +28,7 @@ app.use(timeMiddleware);
 app.use(express.static(path.join(process.cwd(),"public")));
 app.set('views', path.join(process.cwd(), 'src', 'server', 'views'));
 app.set('view engine', 'ejs');
-
+configureSession(app);
 app.use('/', rootRouter);
 app.use('/test',rootRouter);
 
@@ -32,3 +39,8 @@ app.use((_request,_response,next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
+// …other exports
